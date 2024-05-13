86d6f96fcf8dc191621e5235514e05ce78c6228b
rey40fp <rey40fp@gmail.com>
2024-05-12 23:41:41 -0400

    new models and training pipelines ready

--- Models/SRCNN_organized/datasets.py
+++ Models/SRCNN_organized/datasets.py
@@ -2,30 +2,108 @@ import h5py
 import numpy as np
 from torch.utils.data import Dataset
 
+#Original
+# class TrainDataset(Dataset):
+#     def __init__(self, h5_file):
+#         super(TrainDataset, self).__init__()
+#         self.h5_file = h5_file
 
-class TrainDataset(Dataset):
+#     def __getitem__(self, idx):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)
+
+#     def __len__(self):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             return len(f['lr'])
+
+
+#Original
+# class EvalDataset(Dataset):
+#     def __init__(self, h5_file):
+#         super(EvalDataset, self).__init__()
+#         self.h5_file = h5_file
+
+#     def __getitem__(self, idx):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             return np.expand_dims(f['lr'][str(idx)][:, :] / 255., 0), np.expand_dims(f['hr'][str(idx)][:, :] / 255., 0)
+
+#     def __len__(self):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             return len(f['lr'])
+
+
+class TrainDataset_3D(Dataset):
     def __init__(self, h5_file):
-        super(TrainDataset, self).__init__()
+        super(TrainDataset_3D, self).__init__()
         self.h5_file = h5_file
 
     def __getitem__(self, idx):
         with h5py.File(self.h5_file, 'r') as f:
-            return np.expand_dims(f['lr'][idx] / 255., 0), np.expand_dims(f['hr'][idx] / 255., 0)
+            # Handle boundary case; use the same frame as previous if it's the first frame
+            if idx == 0:
+                prev_idx = 0
+            else:
+                prev_idx = idx - 1
+
+            current_lr = np.expand_dims(f['lr'][idx] / 255., 0)
+            prev_lr = np.expand_dims(f['lr'][prev_idx] / 255., 0)
+            
+            # Stack current and previous LR images along the channel dimension to create a 2-channel input
+            input_lr = np.concatenate((current_lr, prev_lr), axis=0)  # Concatenating along the channel axis
+            
+            label_hr = np.expand_dims(f['hr'][idx] / 255., 0)
+
+            return input_lr, label_hr
 
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
             return len(f['lr'])
+        
 
-
-class EvalDataset(Dataset):
+class EvalDataset_3D(Dataset):
     def __init__(self, h5_file):
-        super(EvalDataset, self).__init__()
+        super(EvalDataset_3D, self).__init__()
         self.h5_file = h5_file
 
     def __getitem__(self, idx):
         with h5py.File(self.h5_file, 'r') as f:
-            return np.expand_dims(f['lr'][str(idx)][:, :] / 255., 0), np.expand_dims(f['hr'][str(idx)][:, :] / 255., 0)
+            # Handle boundary case for the first frame
+            if idx == 0:
+                prev_idx = 0
+            else:
+                prev_idx = idx - 1
+            # prev_idx = idx
+
+            # Load the current and previous LR frames and scale them
+            current_lr = np.expand_dims(f['lr'][str(idx)][:,:] / 255., 0)
+            prev_lr = np.expand_dims(f['lr'][str(prev_idx)][:,:] / 255., 0)
+
+            # Concatenate along the channel dimension to form a 2-channel input
+            input_lr = np.concatenate((current_lr, prev_lr), axis=0)
+
+            # Load and scale the HR frame
+            label_hr = np.expand_dims(f['hr'][str(idx)][:,:] / 255., 0)
+
+            return input_lr, label_hr
 
     def __len__(self):
         with h5py.File(self.h5_file, 'r') as f:
             return len(f['lr'])
+        
+
+
+# class EvalDataset_3D(Dataset):
+#     def __init__(self, h5_file):
+#         super(EvalDataset_3D, self).__init__()
+#         self.h5_file = h5_file
+
+#     def __getitem__(self, idx):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             current_lr = np.expand_dims(f['lr'][str(idx)][:, :] / 255., 0)
+#             label_hr = np.expand_dims(f['hr'][str(idx)][:, :] / 255., 0)
+#             current_plus_prior = np.concatenate((current_lr, current_lr), axis=0)
+#             return current_plus_prior, label_hr 
+
+#     def __len__(self):
+#         with h5py.File(self.h5_file, 'r') as f:
+#             return len(f['lr'])
--- Models/SRCNN_organized/models.py
+++ Models/SRCNN_organized/models.py
@@ -14,3 +14,22 @@ class SRCNN(nn.Module):
         x = self.relu(self.conv2(x))
         x = self.conv3(x)
         return x
+
+
+
+
+
+#ver1
+class SRCNN_video(nn.Module):
+    def __init__(self, num_channels=2):
+        super(SRCNN_video, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=5 // 2)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=5 // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
